c427ed76deb8eb7f2e2ca625b03fe9ec5b9ae77b
jhon lawrence <caracoljl@gmail.com>
2026-04-27 21:32:34 +0800

    Fix: port 10000, remove dotenv

--- server/server.js
+++ server/server.js
@@ -192,8 +192,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 const startServer = async () => {
   try {
