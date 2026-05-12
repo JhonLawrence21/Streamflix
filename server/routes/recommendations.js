7f3632d765de71a76e9f48afb25fb5fd63996f6a
Jhon Lawrence <caracoljl@gmail.com>
2026-05-12 12:22:54 +0800

    Fix: Add defensive loading for recommendation controller

--- server/routes/recommendations.js
+++ server/routes/recommendations.js
@@ -2,12 +2,28 @@ const express = require('express');
 const router = express.Router();
 const { auth } = require('../middleware/auth');
 
-const recommendationController = require('../controllers/recommendationController');
+let recommendationController;
+try {
+  recommendationController = require('../controllers/recommendationController');
+  console.log('[recommendations] Controller loaded:', Object.keys(recommendationController));
+} catch (err) {
+  console.error('[recommendations] Controller load error:', err.message);
+}
 
-router.get('/for-you', auth, recommendationController.getForYou);
-router.get('/similar/:movieId', recommendationController.getSimilar);
-router.post('/track/:movieId', auth, recommendationController.trackWatch);
-router.get('/history', auth, recommendationController.getHistory);
-router.delete('/history', auth, recommendationController.clearHistory);
+if (recommendationController && recommendationController.getForYou) {
+  router.get('/for-you', auth, recommendationController.getForYou);
+}
+if (recommendationController && recommendationController.getSimilar) {
+  router.get('/similar/:movieId', recommendationController.getSimilar);
+}
+if (recommendationController && recommendationController.trackWatch) {
+  router.post('/track/:movieId', auth, recommendationController.trackWatch);
+}
+if (recommendationController && recommendationController.getHistory) {
+  router.get('/history', auth, recommendationController.getHistory);
+}
+if (recommendationController && recommendationController.clearHistory) {
+  router.delete('/history', auth, recommendationController.clearHistory);
+}
 
 module.exports = router;
