65629f4158896d191a19cc28ed30ab18b74e3670
Jhon Lawrence <caracoljl@gmail.com>
2026-05-15 18:44:44 +0800

    fix: simplify youtube embed URL to minimal params (autoplay+mute)

--- client/src/pages/MovieDetailsPage.jsx
+++ client/src/pages/MovieDetailsPage.jsx
@@ -130,7 +130,7 @@ const MovieDetailsPage = () => {
         />
         {trailerId && (
           <iframe
-            src={`https://www.youtube.com/embed/${trailerId}?autoplay=1&mute=1&loop=1&playlist=${trailerId}&controls=0&modestbranding=1&rel=0&playsinline=1`}
+            src={`https://www.youtube.com/embed/${trailerId}?autoplay=1&mute=1`}
             className="absolute inset-0 w-full h-[40vh] md:h-[60vh] pointer-events-none"
             style={{ border: 'none' }}
             allow="autoplay; encrypted-media"
